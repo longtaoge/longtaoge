e3c2991c9d878cf777340e4cda0eb2de90548365
longtaoge <61852263@qq.com>
2017-10-09 15:12:23 +0800

    添加

---
+++ calc.js
@@ -0,0 +1,15 @@
+/**
+ * Created by longtaoge on 2017/10/9.
+ */
+exports.add = (numA, numB) => {
+    return numA + numB
+}
+
+exports.sub = (numA, numB) => {
+    return numA - numB
+}
+
+exports.c = (numA, numB) => {
+    return numA * numB
+}
+
---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by longtaoge on 2017/10/9.
+ */
+console.log('这是一个mpm 测试项目');
+
+const calc = require('./calc.js');
+
+module.exports = {
+    calc,
+    msg: '你好,龙套哥!'
+}
+
